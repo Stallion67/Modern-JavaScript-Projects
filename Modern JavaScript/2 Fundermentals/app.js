f0a70c1d568d596f4a8080a8e3e26b2648527f69
Ben Okello Mwaka <mwakaben@gmail.com>
2021-01-24 03:03:09 +0300

    Understanding Varaibles using The key word (var)

---
+++ Modern JavaScript/2 Fundermentals/app.js
@@ -0,0 +1,46 @@
+//var Variables
+
+var learning ='JavaScript Varaible';
+var learning_2 ="JavaScript Naming "
+var day99= 99;
+
+console.log(day99);
+console.log(learning);
+console.log(learning_2);
+
+//Variables with two or more words naming
+
+var firstName='Ben Okello'; //camelCase
+var first_Name='Ben Okello'; //underscore
+var FirstName= 'Okello Mwaka';//pascalCase
+
+console.log(firstName);
+console.log(first_Name);
+console.log(FirstName);
+
+// Other Tyoes of Variable Data types
+
+var age =20;
+var job = true;
+
+console.log(age);
+console.log(job);
+
+/// Adding value to variable
+var  test ;
+test= "Added test to variable";
+console.log(test);
+
+//creating multiple variables
+
+var Name1,Name2,Name3, area;
+
+Name1="Ben";
+Name2="okello";
+Name3='Mwaka';
+area='Entebbe';
+
+console.log(Name2);
+console.log(Name1);
+console.log(Name3);
+console.log(area);
