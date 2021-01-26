35e4b7884da36091281ab93681a218924ae3d471
Ben Okello Mwaka <mwakaben@gmail.com>
2021-01-26 20:36:14 +0300

    Data types in Javascript

---
+++ Modern JavaScript/2 Fundermentals/2.2 Data Types/app.js
@@ -0,0 +1,11 @@
+let variable='Ben Okello';
+//changing the datatype by assinging anothter Value
+variable=20;  //number
+variable=-150.88;  //Float, decimal , int aare all started as numbers
+variable=true; //boolean
+variable= undefined; //undefined
+variable=Symbol('My new symbol') //symbol
+
+
+console.log(variable);
+console.log(typeof variable);
