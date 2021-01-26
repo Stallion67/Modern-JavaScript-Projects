3fe20afd82ce97853c7d98e0c4f77ffed760bc50
Ben Okello Mwaka <mwakaben@gmail.com>
2021-01-26 21:35:38 +0300

    String Data types/ concatenating

--- Modern JavaScript/2 Fundermentals/2.2 Data Types/app.js
+++ Modern JavaScript/2 Fundermentals/2.2 Data Types/app.js
@@ -1,11 +1,37 @@
-let variable='Ben Okello';
+let variable='Ben Okello';//String
+variable='20';  //number
+
 //changing the datatype by assinging anothter Value
 variable=20;  //number
 variable=-150.88;  //Float, decimal , int aare all started as numbers
-variable=true; //boolean
-variable= undefined; //undefined
-variable=Symbol('My new symbol') //symbol
+variable=20;  //number
 
+//boolean
+variable=true; 
+
+//undefined
+variable= undefined; 
+let person; //resturns null since it is not assigned a value
+
+//symbol
+variable=Symbol('My new symbol') 
+
+//Null
+variable=null;  //null are treated as objects
+
+//Arrays
+let ProgramingLanguage=['Html','Js','PHP','C#']; //Arrays are of Type Object
+
+//Objects
+let person={
+    name:'Ben Okello Mwaka',
+    country:'Mexico',
+    region:'Central',
+    city:'Entebbe'
+}
+
+//Dates
+let today= new Date(); /// Dates are also of datatype Object
 
 console.log(variable);
 console.log(typeof variable);
---
+++ Modern JavaScript/2 Fundermentals/2.3 Strings/app.js
@@ -0,0 +1,48 @@
+/*
+Mutli-Line Comment
+*/
+
+let name;
+name='Ben Okello'
+name="Ben Okello Mwaka"
+
+//when Wrinting a string with a qoute in it
+let message = "Then I said: hey that's nice";
+
+console.log(name);
+console.log(message);
+
+//concatenation
+ let learning ="JavaScript",
+ modern ="mordern"
+
+ console.log('learning: '+modern+' '+learning);
+
+ let learning2="Mordern"+ " "+'Javscript';
+ console.log(learning2);
+console.log(2+2);// concTENATING 2 NUMBERS ADDS THEM.
+
+//cONCATENATING 2 VARIABLES
+ let learning3='learning',
+ tech='JavaScript';
+
+ console.log(learning3+' '+tech);
+
+ //concatenate Varaibles with strings
+ let band='Metallica',
+ song='Enter Sandman';
+
+ let playing;
+
+ playing='Now Playing: '+song+' :by-'+band;
+ console.log(playing);
+
+ //Appending to a vairable
+ let studying;
+
+ studying='Modern';
+ studying+=' '+'JavaScript';
+ studying += " in 100 days"
+
+ console.log('I am studying'+' '+studying);
+
